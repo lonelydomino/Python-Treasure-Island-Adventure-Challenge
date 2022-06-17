6be447997b03f2ce94e754b17760c8be8fa4d5d2
Michael Martinez <66656582+lonelydomino@users.noreply.github.com>
2022-06-17 06:39:16 +0000

    Finishing

--- main.py
+++ main.py
@@ -30,9 +30,15 @@ print("Your mission is to find the treasure.")
 choice = input("You're at a crossroad. Where do you want to go? Type \"left\" or \"right\"").lower()
 
 if choice == 'left':
-  choice = input("You've come to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim across.")
+  print("You've come to a lake. There is an island in the middle of the lake.")
+  choice = input("Type wait to wait for a boat. Type swim to swim across.")
   if choice == 'wait':
-    print("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?")
+    print("You arrive at the island unharmed. There is a house with 3 doors.")
+    choice = input(" One red, one yellow and one blue. Which colour do you choose?")
+    if choice == 'yellow':
+      print("You found the treasure! You win!")
+    else:
+      print("Game Over!")
   else:
     print('Game Over!')
 else:
